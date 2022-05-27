fccf92339ddaf3cda42af6b4f6b3d1bd5cc20718
Your Name <you@example.com>
2022-05-27 10:12:39 +0800

    更改本地api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://netease-cloud-music-api-sooty-phi.vercel.app/';
 export default axios;
